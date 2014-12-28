1c84b8b1feb2f3a540a73caa812ce82dbe0a646a
Hideaki Hagiwara <Hideaki Hagiwara@localhost>
2014-12-28 23:11:27 +0900

    ????

---
+++ jpiere.base.plugin/src/jpiere/base/plugin/Factory/JPiereBasePluginFormFactory.java
@@ -0,0 +1,57 @@
+package jpiere.base.plugin.Factory;
+
+import java.util.logging.Level;
+
+import org.adempiere.webui.factory.IFormFactory;
+import org.adempiere.webui.panel.ADForm;
+import org.adempiere.webui.panel.IFormController;
+import org.compiere.util.CLogger;
+
+public class JPiereBasePluginFormFactory implements IFormFactory{
+
+	private static final CLogger log = CLogger.getCLogger(JPiereBasePluginFormFactory.class);
+
+	@Override
+	public ADForm newFormInstance(String formName) {
+		Object form = null;
+	     if (formName.startsWith("jpiere.base.plugin.webui.apps.form")) {
+	           ClassLoader cl = getClass().getClassLoader();
+	           Class<?> clazz = null;
+
+			  try
+			  {
+				clazz = cl.loadClass(formName);
+		      }
+			  catch (Exception e)
+			  {
+			    if (log.isLoggable(Level.INFO))
+			       log.log(Level.INFO, e.getLocalizedMessage(), e);
+		            return null;
+			  }
+		         try
+			  {
+			    form = clazz.newInstance();
+			  }
+			  catch (Exception e)
+			  {
+			     if (log.isLoggable(Level.WARNING))
+				log.log(Level.WARNING, e.getLocalizedMessage(), e);
+		      }
+
+		      if (form != null) {
+			     if (form instanceof ADForm) {
+			    	 return (ADForm)form;
+			     }
+			     else if (form instanceof IFormController) {
+					IFormController controller = (IFormController) form;
+					ADForm adForm = controller.getForm();
+					adForm.setICustomForm(controller);
+					return adForm;
+			     }
+		     }
+	     }
+	     return null;
+	}
+
+
+}
