fe28f8939d05280f93ba616b4f2bb34bae8b8b20
Hideaki Hagiwara <Hideaki Hagiwara@localhost>
2015-12-26 13:49:20 +0900

    JPIERE-0142

--- jpiere.base.plugin/src/jpiere/base/plugin/factory/JPiereBasePluginModelFactory.java
+++ jpiere.base.plugin/src/jpiere/base/plugin/factory/JPiereBasePluginModelFactory.java
@@ -22,9 +22,11 @@ import jpiere.base.plugin.org.adempiere.model.MBillSchema;
 import jpiere.base.plugin.org.adempiere.model.MCorporation;
 import jpiere.base.plugin.org.adempiere.model.MCorporationGroup;
 import jpiere.base.plugin.org.adempiere.model.MGroupCorporations;
+import jpiere.base.plugin.org.adempiere.model.MOrderJP;
 import jpiere.base.plugin.org.adempiere.model.MReferenceTest;
 
 import org.adempiere.base.IModelFactory;
+import org.compiere.model.MOrder;
 import org.compiere.model.PO;
 import org.compiere.util.Env;
 
@@ -41,20 +43,29 @@ public class JPiereBasePluginModelFactory implements IModelFactory {
 
 	@Override
 	public Class<?> getClass(String tableName) {
-		if(tableName.equals(MCorporation.Table_Name)){
-			return MCorporation.class;
-		}else if(tableName.equals(MCorporationGroup.Table_Name)){
-			return MCorporationGroup.class;
-		}else if(tableName.equals(MGroupCorporations.Table_Name)){
-			return MGroupCorporations.class;
-		}else if(tableName.equals(MBill.Table_Name)){
-			return MBill.class;
-		}else if(tableName.equals(MBillLine.Table_Name)){
-			return MBillLine.class;
-		}else if(tableName.equals(MReferenceTest.Table_Name)){
-			return MReferenceTest.class;
-		}else if(tableName.equals(MBillSchema.Table_Name)){
-			return MBillSchema.class;
+		
+		if(tableName.startsWith("JP"))
+		{
+			if(tableName.equals(MCorporation.Table_Name)){
+				return MCorporation.class;
+			}else if(tableName.equals(MCorporationGroup.Table_Name)){
+				return MCorporationGroup.class;
+			}else if(tableName.equals(MGroupCorporations.Table_Name)){
+				return MGroupCorporations.class;
+			}else if(tableName.equals(MBill.Table_Name)){
+				return MBill.class;
+			}else if(tableName.equals(MBillLine.Table_Name)){
+				return MBillLine.class;
+			}else if(tableName.equals(MReferenceTest.Table_Name)){
+				return MReferenceTest.class;
+			}else if(tableName.equals(MBillSchema.Table_Name)){
+				return MBillSchema.class;
+			}
+			
+		}else{
+			if(tableName.equals(MOrder.Table_Name)){
+				return MOrderJP.class;
+			}
 		}
 
 		return null;
@@ -62,42 +73,60 @@ public class JPiereBasePluginModelFactory implements IModelFactory {
 
 	@Override
 	public PO getPO(String tableName, int Record_ID, String trxName) {
-		if(tableName.equals(MCorporation.Table_Name)){
-			return  new MCorporation(Env.getCtx(), Record_ID, trxName);
-		}else if(tableName.equals(MCorporationGroup.Table_Name)){
-			return  new MCorporationGroup(Env.getCtx(), Record_ID, trxName);
-		}else if(tableName.equals(MGroupCorporations.Table_Name)){
-			return  new MGroupCorporations(Env.getCtx(), Record_ID, trxName);
-		}else if(tableName.equals(MBill.Table_Name)){
-			return  new MBill(Env.getCtx(), Record_ID, trxName);
-		}else if(tableName.equals(MBillLine.Table_Name)){
-			return  new MBillLine(Env.getCtx(), Record_ID, trxName);
-		}else if(tableName.equals(MReferenceTest.Table_Name)){
-			return  new MReferenceTest(Env.getCtx(), Record_ID, trxName);
-		}else if(tableName.equals(MBillSchema.Table_Name)){
-			return  new MBillSchema(Env.getCtx(), Record_ID, trxName);
+		
+		if(tableName.startsWith("JP"))
+		{
+		
+			if(tableName.equals(MCorporation.Table_Name)){
+				return  new MCorporation(Env.getCtx(), Record_ID, trxName);
+			}else if(tableName.equals(MCorporationGroup.Table_Name)){
+				return  new MCorporationGroup(Env.getCtx(), Record_ID, trxName);
+			}else if(tableName.equals(MGroupCorporations.Table_Name)){
+				return  new MGroupCorporations(Env.getCtx(), Record_ID, trxName);
+			}else if(tableName.equals(MBill.Table_Name)){
+				return  new MBill(Env.getCtx(), Record_ID, trxName);
+			}else if(tableName.equals(MBillLine.Table_Name)){
+				return  new MBillLine(Env.getCtx(), Record_ID, trxName);
+			}else if(tableName.equals(MReferenceTest.Table_Name)){
+				return  new MReferenceTest(Env.getCtx(), Record_ID, trxName);
+			}else if(tableName.equals(MBillSchema.Table_Name)){
+				return  new MBillSchema(Env.getCtx(), Record_ID, trxName);
+			}
+			
+		}else{
+			if(tableName.equals(MOrder.Table_Name)){
+				return  new MOrderJP(Env.getCtx(), Record_ID, trxName);
+			}		
 		}
-
 
 		return null;
 	}
 
 	@Override
 	public PO getPO(String tableName, ResultSet rs, String trxName) {
-		if(tableName.equals(MCorporation.Table_Name)){
-			return  new MCorporation(Env.getCtx(), rs, trxName);
-		}else if(tableName.equals(MCorporationGroup.Table_Name)){
-			return  new MCorporationGroup(Env.getCtx(), rs, trxName);
-		}else if(tableName.equals(MGroupCorporations.Table_Name)){
-			return  new MGroupCorporations(Env.getCtx(), rs, trxName);
-		}else if(tableName.equals(MBill.Table_Name)){
-			return  new MBill(Env.getCtx(), rs, trxName);
-		}else if(tableName.equals(MBillLine.Table_Name)){
-			return  new MBillLine(Env.getCtx(), rs, trxName);
-		}else if(tableName.equals(MReferenceTest.Table_Name)){
-			return  new MReferenceTest(Env.getCtx(), rs, trxName);
-		}else if(tableName.equals(MBillSchema.Table_Name)){
-			return  new MBillSchema(Env.getCtx(), rs, trxName);
+		
+		if(tableName.startsWith("JP"))
+		{
+		
+			if(tableName.equals(MCorporation.Table_Name)){
+				return  new MCorporation(Env.getCtx(), rs, trxName);
+			}else if(tableName.equals(MCorporationGroup.Table_Name)){
+				return  new MCorporationGroup(Env.getCtx(), rs, trxName);
+			}else if(tableName.equals(MGroupCorporations.Table_Name)){
+				return  new MGroupCorporations(Env.getCtx(), rs, trxName);
+			}else if(tableName.equals(MBill.Table_Name)){
+				return  new MBill(Env.getCtx(), rs, trxName);
+			}else if(tableName.equals(MBillLine.Table_Name)){
+				return  new MBillLine(Env.getCtx(), rs, trxName);
+			}else if(tableName.equals(MReferenceTest.Table_Name)){
+				return  new MReferenceTest(Env.getCtx(), rs, trxName);
+			}else if(tableName.equals(MBillSchema.Table_Name)){
+				return  new MBillSchema(Env.getCtx(), rs, trxName);
+			}
+		}else{
+			if(tableName.equals(MOrder.Table_Name)){
+				return  new MOrderJP(Env.getCtx(), rs, trxName);
+			}		
 		}
 
 		return null;
---
+++ jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/model/MOrderJP.java
@@ -0,0 +1,63 @@
+/******************************************************************************
+ * Product: JPiere                                                            *
+ * Copyright (C) Hideaki Hagiwara (h.hagiwara@oss-erp.co.jp)                  *
+ *                                                                            *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY.                          *
+ * See the GNU General Public License for more details.                       *
+ *                                                                            *
+ * JPiere is maintained by OSS ERP Solutions Co., Ltd.                        *
+ * (http://www.oss-erp.co.jp)                                                 *
+ *****************************************************************************/
+package jpiere.base.plugin.org.adempiere.model;
+
+import java.sql.ResultSet;
+import java.util.Properties;
+
+import org.compiere.model.MDocType;
+import org.compiere.model.MOrder;
+import org.compiere.model.MProject;
+import org.compiere.process.DocOptions;
+import org.compiere.process.DocumentEngine;
+
+public class MOrderJP extends MOrder implements DocOptions {
+	
+	public MOrderJP(Properties ctx, int C_Order_ID, String trxName) {
+		super(ctx, C_Order_ID, trxName);
+	}
+	
+	public MOrderJP(MProject project, boolean IsSOTrx, String DocSubTypeSO) {
+		super(project, IsSOTrx, DocSubTypeSO);
+	}
+	
+	public MOrderJP(Properties ctx, ResultSet rs, String trxName) {
+		super(ctx, rs, trxName);
+	}
+	
+	@Override
+	public int customizeValidActions(String docStatus, Object processing, String orderType, String isSOTrx,
+			int AD_Table_ID, String[] docAction, String[] options, int index) {
+		
+		if(isSOTrx.equals("Y") && (orderType.equals(MDocType.DOCSUBTYPESO_Proposal)
+				|| orderType.equals(MDocType.DOCSUBTYPESO_Quotation)))
+		{
+			index = 0; //initialize the index
+			options[index++] = DocumentEngine.ACTION_Prepare; 
+			options[index++] = DocumentEngine.ACTION_Void; 
+			return index;
+		}
+		
+		if (docStatus.equals(DocumentEngine.STATUS_Completed)) 
+		{
+			index = 0; //initialize the index
+			options[index++] = DocumentEngine.ACTION_Close; 
+			options[index++] = DocumentEngine.ACTION_ReActivate;
+			return index;
+		}
+		
+		return index;
+	}
+	
+}
