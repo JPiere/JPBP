dcf9fc838900976f0e082ff00c4357971b63f533
Hideaki Hagiwara <Hideaki Hagiwara@localhost>
2016-07-09 17:50:31 +0900

    JPIERE-0185

--- jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/process/CreateSOfromEstimation.java
+++ jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/process/CreateSOfromEstimation.java
@@ -19,12 +19,16 @@ import java.util.logging.Level;
 import jpiere.base.plugin.org.adempiere.model.MEstimation;
 import jpiere.base.plugin.org.adempiere.model.MEstimationLine;
 
+import org.adempiere.util.Callback;
+import org.adempiere.util.IProcessUI;
 import org.compiere.model.MOrder;
 import org.compiere.model.MOrderLine;
 import org.compiere.model.PO;
 import org.compiere.process.DocAction;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
+import org.compiere.util.Env;
+import org.compiere.util.Msg;
 
 /**
  * JPIERE-0185 : Create SO from Estimation
@@ -36,11 +40,14 @@ public class CreateSOfromEstimation extends SvrProcess {
 
 	private int			p_JP_Estimation_ID = 0;
 	private String		p_DocAction = null;
-
+	private MEstimation estimation = null;
+	IProcessUI processUI = null;
+	
 	@Override
 	protected void prepare() {
 
 		p_JP_Estimation_ID = getRecord_ID();
+
 		
 		ProcessInfoParameter[] para = getParameter();
 		for (int i = 0; i < para.length; i++)
@@ -55,17 +62,37 @@ public class CreateSOfromEstimation extends SvrProcess {
 			}//if
 
 		}//for
+		
+		processUI = Env.getProcessUI(getCtx());
+		estimation = new MEstimation(getCtx(), p_JP_Estimation_ID, get_TrxName()) ;
+		if(processUI != null && estimation.getLink_Order_ID() != 0)
+		{
+			//Already Sales Order created, Do you want to create Sales Order again?
+			processUI.ask(Msg.getMsg(getCtx(), "JP_CreateSOfromEstimationAgain"), new Callback<Boolean>() {
+
+				@Override
+				public void onCallback(Boolean result)
+				{
+					if (result)
+					{
+						createSO();
+					}else{
+						;
+					}
+		        }
+
+			});//FDialog.
+		}
 	}
 
-	@Override
-	protected String doIt() throws Exception 
+	private String createSO() 
 	{
 
-		MEstimation estimation = new MEstimation(getCtx(), p_JP_Estimation_ID, get_TrxName()) ;
 		MEstimationLine[] eLines = estimation.getLines();
 		
 		MOrder order = new MOrder(getCtx(), 0, get_TrxName()) ;
 		PO.copyValues(estimation, order);
+		order.setAD_Org_ID(estimation.getAD_Org_ID());
 		order.setDocumentNo(null);
 		order.setC_DocTypeTarget_ID(estimation.getC_DocType_ID());
 		order.setDocStatus(DocAction.STATUS_Drafted);
@@ -78,7 +105,8 @@ public class CreateSOfromEstimation extends SvrProcess {
 		for(int i = 0; i < eLines.length; i++)
 		{
 			MOrderLine oLine = new MOrderLine(order);
-			PO.copyValues(eLines[i], oLine);			
+			PO.copyValues(eLines[i], oLine);	
+			oLine.setAD_Org_ID(eLines[i].getAD_Org_ID());
 			oLine.saveEx(get_TrxName());
 			
 		}//for
@@ -86,7 +114,18 @@ public class CreateSOfromEstimation extends SvrProcess {
 		order.processIt(p_DocAction);
 		order.saveEx(get_TrxName());
 		
-		return "OK";
+		return order.getDocumentInfo();
+	}
+	
+	@Override
+	protected String doIt() throws Exception 
+	{
+		if(processUI == null || estimation.getLink_Order_ID() == 0)
+		{
+			return createSO();
+		}
+		
+		return "";
 	}
 
 }
