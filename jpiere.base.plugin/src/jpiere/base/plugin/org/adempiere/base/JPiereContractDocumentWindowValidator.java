00916e6bf05a86e3b24d1702c865f00597729a1a
Hideaki Hagiwara <hagiwara.hideaki@gmail.com>
2017-10-09 17:43:41 +0900

    #106 JPIERE-0363 Contract Docuemnt Window Validator

--- jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/base/JPiereContractDocumentWindowValidator.java
+++ jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/base/JPiereContractDocumentWindowValidator.java
@@ -14,11 +14,14 @@
 
 package jpiere.base.plugin.org.adempiere.base;
 
+import java.sql.Timestamp;
+
 import org.adempiere.util.Callback;
 import org.adempiere.webui.adwindow.validator.WindowValidator;
 import org.adempiere.webui.adwindow.validator.WindowValidatorEvent;
 import org.adempiere.webui.adwindow.validator.WindowValidatorEventType;
 import org.adempiere.webui.window.FDialog;
+import org.compiere.model.GridField;
 import org.compiere.model.GridTab;
 import org.compiere.util.Env;
 import org.compiere.util.Msg;
@@ -37,7 +40,7 @@ public class JPiereContractDocumentWindowValidator implements WindowValidator {
 	public void onWindowEvent(WindowValidatorEvent event, Callback<Boolean> callback) 
 	{
 		
-		if(event.getName().equals(WindowValidatorEventType.AFTER_SAVE.getName()))
+		if(event.getName().equals(WindowValidatorEventType.BEFORE_SAVE.getName()))
 		{
 			GridTab gridTab =event.getWindow().getADWindowContent().getActiveGridTab();
 			if(gridTab.getTabNo() == 0 )
@@ -47,13 +50,46 @@ public class JPiereContractDocumentWindowValidator implements WindowValidator {
 				int Record_ID = gridTab.getRecord_ID();
 				if(JP_ContractType.equals("PDC") && Record_ID > 0)
 				{
-					MContract contract = MContract.get(Env.getCtx(), Record_ID);
-					if(contract.getContractContents(true,null).length > 0)
+					GridField gf_ContractPeriodDate_To = gridTab.getField("JP_ContractPeriodDate_To");
+					if(gf_ContractPeriodDate_To != null)
 					{
-						FDialog.info(event.getWindow().getADWindowContent().getWindowNo()
-								, event.getWindow().getComponent(), "JP_ToBeConfirmed", Msg.getElement(Env.getCtx(), "JP_ConfirmContractProcessDateTo"));
+						Timestamp old_ContractPeriodDate_To = null;
+						Timestamp new_ContractPeriodDate_To = null;
+						Object old_value = gf_ContractPeriodDate_To.getOldValue();
+						Object new_value = gf_ContractPeriodDate_To.getValue();
+						if(old_value == null)
+							old_ContractPeriodDate_To = null;
+						else
+							old_ContractPeriodDate_To = (Timestamp)old_value;
 						
-					}
+						if(new_value == null)
+							new_ContractPeriodDate_To = null;
+						else
+							new_ContractPeriodDate_To = (Timestamp)new_value;
+						
+						if(old_ContractPeriodDate_To != null && new_ContractPeriodDate_To != null
+								&& old_ContractPeriodDate_To.compareTo(new_ContractPeriodDate_To) != 0 )
+						{
+							MContract contract = MContract.get(Env.getCtx(), Record_ID);
+							if(contract.getContractContents(true,null).length > 0)
+							{
+								FDialog.info(event.getWindow().getADWindowContent().getWindowNo()
+										, event.getWindow().getComponent(), "JP_ToBeConfirmed", Msg.getMsg(Env.getCtx(), "JP_ConfirmContractProcessDateTo"));
+							}
+						}else if(old_ContractPeriodDate_To == null && new_ContractPeriodDate_To == null){
+							
+							;//Noting to do;
+							
+						}else if(old_ContractPeriodDate_To == null || new_ContractPeriodDate_To == null){
+							MContract contract = MContract.get(Env.getCtx(), Record_ID);
+							if(contract.getContractContents(true,null).length > 0)
+							{
+								FDialog.info(event.getWindow().getADWindowContent().getWindowNo()
+										, event.getWindow().getComponent(), "JP_ToBeConfirmed", Msg.getMsg(Env.getCtx(), "JP_ConfirmContractProcessDateTo"));
+							}
+						}
+						
+					}//if(gf_ContractPeriodDate_To != null)
 					
 				}//if(JP_ContractType.equals("PD"))
 				
