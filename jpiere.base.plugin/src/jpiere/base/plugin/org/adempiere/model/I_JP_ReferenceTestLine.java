72d4a752cb3ecf2ce388e327f6571c2b040766ec
jpiere <h.hagiwara@compiere-distribution-lab.net>
2020-03-05 23:19:36 +0900

    JPIERE-0084 All Reference Test update

---
+++ jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/model/I_JP_ReferenceTestLine.java
@@ -0,0 +1,179 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package jpiere.base.plugin.org.adempiere.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import org.compiere.model.*;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for JP_ReferenceTestLine
+ *  @author iDempiere (generated) 
+ *  @version Release 7.1
+ */
+@SuppressWarnings("all")
+public interface I_JP_ReferenceTestLine 
+{
+
+    /** TableName=JP_ReferenceTestLine */
+    public static final String Table_Name = "JP_ReferenceTestLine";
+
+    /** AD_Table_ID=1000236 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 7 - System - Client - Org 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(7);
+
+    /** Load Meta Data */
+
+    /** Column name AD_Client_ID */
+    public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/** Get Client.
+	  * Client/Tenant for this installation.
+	  */
+	public int getAD_Client_ID();
+
+    /** Column name AD_Org_ID */
+    public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/** Set Organization.
+	  * Organizational entity within client
+	  */
+	public void setAD_Org_ID (int AD_Org_ID);
+
+	/** Get Organization.
+	  * Organizational entity within client
+	  */
+	public int getAD_Org_ID();
+
+    /** Column name Created */
+    public static final String COLUMNNAME_Created = "Created";
+
+	/** Get Created.
+	  * Date this record was created
+	  */
+	public Timestamp getCreated();
+
+    /** Column name CreatedBy */
+    public static final String COLUMNNAME_CreatedBy = "CreatedBy";
+
+	/** Get Created By.
+	  * User who created this records
+	  */
+	public int getCreatedBy();
+
+    /** Column name Description */
+    public static final String COLUMNNAME_Description = "Description";
+
+	/** Set Description.
+	  * Optional short description of the record
+	  */
+	public void setDescription (String Description);
+
+	/** Get Description.
+	  * Optional short description of the record
+	  */
+	public String getDescription();
+
+    /** Column name IsActive */
+    public static final String COLUMNNAME_IsActive = "IsActive";
+
+	/** Set Active.
+	  * The record is active in the system
+	  */
+	public void setIsActive (boolean IsActive);
+
+	/** Get Active.
+	  * The record is active in the system
+	  */
+	public boolean isActive();
+
+    /** Column name JP_ReferenceTestLine_ID */
+    public static final String COLUMNNAME_JP_ReferenceTestLine_ID = "JP_ReferenceTestLine_ID";
+
+	/** Set JP_ReferenceTestLine	  */
+	public void setJP_ReferenceTestLine_ID (int JP_ReferenceTestLine_ID);
+
+	/** Get JP_ReferenceTestLine	  */
+	public int getJP_ReferenceTestLine_ID();
+
+    /** Column name JP_ReferenceTestLine_UU */
+    public static final String COLUMNNAME_JP_ReferenceTestLine_UU = "JP_ReferenceTestLine_UU";
+
+	/** Set JP_ReferenceTestLine_UU	  */
+	public void setJP_ReferenceTestLine_UU (String JP_ReferenceTestLine_UU);
+
+	/** Get JP_ReferenceTestLine_UU	  */
+	public String getJP_ReferenceTestLine_UU();
+
+    /** Column name JP_ReferenceTest_ID */
+    public static final String COLUMNNAME_JP_ReferenceTest_ID = "JP_ReferenceTest_ID";
+
+	/** Set JP_ReferenceTest	  */
+	public void setJP_ReferenceTest_ID (int JP_ReferenceTest_ID);
+
+	/** Get JP_ReferenceTest	  */
+	public int getJP_ReferenceTest_ID();
+
+	public I_JP_ReferenceTest getJP_ReferenceTest() throws RuntimeException;
+
+    /** Column name Line */
+    public static final String COLUMNNAME_Line = "Line";
+
+	/** Set Line No.
+	  * Unique line for this document
+	  */
+	public void setLine (int Line);
+
+	/** Get Line No.
+	  * Unique line for this document
+	  */
+	public int getLine();
+
+    /** Column name Name */
+    public static final String COLUMNNAME_Name = "Name";
+
+	/** Set Name.
+	  * Alphanumeric identifier of the entity
+	  */
+	public void setName (String Name);
+
+	/** Get Name.
+	  * Alphanumeric identifier of the entity
+	  */
+	public String getName();
+
+    /** Column name Updated */
+    public static final String COLUMNNAME_Updated = "Updated";
+
+	/** Get Updated.
+	  * Date this record was updated
+	  */
+	public Timestamp getUpdated();
+
+    /** Column name UpdatedBy */
+    public static final String COLUMNNAME_UpdatedBy = "UpdatedBy";
+
+	/** Get Updated By.
+	  * User who updated this records
+	  */
+	public int getUpdatedBy();
+}
