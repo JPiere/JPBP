b70819f4ce9380f0595d30d1b035e4c5433c7a9f
jpiere <h.hagiwara@compiere-distribution-lab.net>
2022-08-20 13:48:52 +0900

    Bug fix - JPIERE-0407:Import GL Journal : DELETE FROM

--- jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/process/JPiereImportGLJournal.java
+++ jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/process/JPiereImportGLJournal.java
@@ -131,7 +131,7 @@ public class JPiereImportGLJournal extends SvrProcess  implements ImportProcess
 		/** Delete Old Imported data */
 		if (p_deleteOldImported)
 		{
-			sql = new StringBuilder ("DELETE I_GLJournalJP ")
+			sql = new StringBuilder ("DELETE FROM I_GLJournalJP ")
 				  .append("WHERE I_IsImported='Y'").append (clientCheck);
 			no = DB.executeUpdate(sql.toString(), get_TrxName());
 			if (log.isLoggable(Level.FINE)) log.fine("Delete Old Impored =" + no);
