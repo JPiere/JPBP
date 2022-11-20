2c3a81ffb8aae3d9d09a300de675b897d71209e0
jpiere <h.hagiwara@compiere-distribution-lab.net>
2022-11-20 18:30:44 +0900

    A little patch - IDEMPIERE-4083:currency rate by document or by transaction

--- jpiere.base.plugin/src/jpiere/base/plugin/org/compiere/acct/DocLine_AllocationJP.java
+++ jpiere.base.plugin/src/jpiere/base/plugin/org/compiere/acct/DocLine_AllocationJP.java
@@ -18,6 +18,7 @@ import java.math.BigDecimal;
 import org.compiere.acct.Doc;
 import org.compiere.acct.DocLine_Allocation;
 import org.compiere.model.MAllocationLine;
+import org.compiere.util.DB;
 
 /**
  * IDEMPIERE-4083:currency rate by document or by transaction
@@ -42,6 +43,29 @@ public class DocLine_AllocationJP extends DocLine_Allocation {
 	{
 		super.setCurrencyRate(currencyRate);
 	}
+
+	
+	/**
+	 * 	Get Invoice C_Currency_ID
+	 * 
+	 * 継承しているDocLine_Allocationが、trxNameをNullで処理しており、Invoiceの登録からAllocationの転記処理まで一連のトランザクションで処理している場合、
+	 * 正しい通貨が取得できず、為替差損益の処理が実行されてしまい、エラーが表示される。
+	 * POS受注などの場合が該当する。
+	 * iDempiereの本家が修正されるまの一時的なパッチとしてオーバーライド修正する。
+	 * 
+	 *	@return 0 if no invoice -1 if not found
+	 */
+	@Override
+	public int getInvoiceC_Currency_ID()
+	{
+		if (getC_Invoice_ID() == 0)
+			return 0;
+		String sql = "SELECT C_Currency_ID "
+			+ "FROM C_Invoice "
+			+ "WHERE C_Invoice_ID=?";
+		return  DB.getSQLValue(getPO().get_TrxName(), sql, getC_Invoice_ID());
+
+	}	//	getInvoiceC_Currency_ID
 
 
 
