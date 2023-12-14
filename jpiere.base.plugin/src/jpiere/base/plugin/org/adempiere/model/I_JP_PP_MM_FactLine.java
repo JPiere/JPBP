2ec406986db027877caa71af9f5a1afb584547c5
OES-MAIN-PC\hhagi <h.hagiwara@ha.hocoroco.ne.jp>
2023-12-14 10:26:49 +0900

    JPIERE-0609 Work Process Management and Create Movement doc from PP Doc

---
+++ jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/model/I_JP_PP_MM_FactLine.java
@@ -0,0 +1,306 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package jpiere.base.plugin.org.adempiere.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import org.compiere.model.*;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for JP_PP_MM_FactLine
+ *  @author iDempiere (generated) 
+ *  @version Release 10
+ */
+@SuppressWarnings("all")
+public interface I_JP_PP_MM_FactLine 
+{
+
+    /** TableName=JP_PP_MM_FactLine */
+    public static final String Table_Name = "JP_PP_MM_FactLine";
+
+    /** AD_Table_ID=1000311 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 1 - Org 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(1);
+
+    /** Load Meta Data */
+
+    /** Column name AD_Client_ID */
+    public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/** Get Tenant.
+	  * Tenant for this installation.
+	  */
+	public int getAD_Client_ID();
+
+    /** Column name AD_Org_ID */
+    public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/** Set Organization.
+	  * Organizational entity within tenant
+	  */
+	public void setAD_Org_ID (int AD_Org_ID);
+
+	/** Get Organization.
+	  * Organizational entity within tenant
+	  */
+	public int getAD_Org_ID();
+
+    /** Column name Created */
+    public static final String COLUMNNAME_Created = "Created";
+
+	/** Get Created.
+	  * Date this record was created
+	  */
+	public Timestamp getCreated();
+
+    /** Column name CreatedBy */
+    public static final String COLUMNNAME_CreatedBy = "CreatedBy";
+
+	/** Get Created By.
+	  * User who created this records
+	  */
+	public int getCreatedBy();
+
+    /** Column name Description */
+    public static final String COLUMNNAME_Description = "Description";
+
+	/** Set Description.
+	  * Optional short description of the record
+	  */
+	public void setDescription (String Description);
+
+	/** Get Description.
+	  * Optional short description of the record
+	  */
+	public String getDescription();
+
+    /** Column name IsActive */
+    public static final String COLUMNNAME_IsActive = "IsActive";
+
+	/** Set Active.
+	  * The record is active in the system
+	  */
+	public void setIsActive (boolean IsActive);
+
+	/** Get Active.
+	  * The record is active in the system
+	  */
+	public boolean isActive();
+
+    /** Column name JP_CommunicationColumn */
+    public static final String COLUMNNAME_JP_CommunicationColumn = "JP_CommunicationColumn";
+
+	/** Set Communication Column	  */
+	public void setJP_CommunicationColumn (String JP_CommunicationColumn);
+
+	/** Get Communication Column	  */
+	public String getJP_CommunicationColumn();
+
+    /** Column name JP_PP_Fact_ID */
+    public static final String COLUMNNAME_JP_PP_Fact_ID = "JP_PP_Fact_ID";
+
+	/** Set PP Fact.
+	  * JPIERE-0501:JPBP
+	  */
+	public void setJP_PP_Fact_ID (int JP_PP_Fact_ID);
+
+	/** Get PP Fact.
+	  * JPIERE-0501:JPBP
+	  */
+	public int getJP_PP_Fact_ID();
+
+	public I_JP_PP_Fact getJP_PP_Fact() throws RuntimeException;
+
+    /** Column name JP_PP_MM_FactLine_ID */
+    public static final String COLUMNNAME_JP_PP_MM_FactLine_ID = "JP_PP_MM_FactLine_ID";
+
+	/** Set JP_PP_MM_FactLine	  */
+	public void setJP_PP_MM_FactLine_ID (int JP_PP_MM_FactLine_ID);
+
+	/** Get JP_PP_MM_FactLine	  */
+	public int getJP_PP_MM_FactLine_ID();
+
+    /** Column name JP_PP_MM_FactLine_UU */
+    public static final String COLUMNNAME_JP_PP_MM_FactLine_UU = "JP_PP_MM_FactLine_UU";
+
+	/** Set JP_PP_MM_FactLine_UU	  */
+	public void setJP_PP_MM_FactLine_UU (String JP_PP_MM_FactLine_UU);
+
+	/** Get JP_PP_MM_FactLine_UU	  */
+	public String getJP_PP_MM_FactLine_UU();
+
+    /** Column name JP_PP_MM_PlanLine_ID */
+    public static final String COLUMNNAME_JP_PP_MM_PlanLine_ID = "JP_PP_MM_PlanLine_ID";
+
+	/** Set JP_PP_MM_PlanLine	  */
+	public void setJP_PP_MM_PlanLine_ID (int JP_PP_MM_PlanLine_ID);
+
+	/** Get JP_PP_MM_PlanLine	  */
+	public int getJP_PP_MM_PlanLine_ID();
+
+	public I_JP_PP_MM_PlanLine getJP_PP_MM_PlanLine() throws RuntimeException;
+
+    /** Column name Line */
+    public static final String COLUMNNAME_Line = "Line";
+
+	/** Set Line No.
+	  * Unique line for this document
+	  */
+	public void setLine (int Line);
+
+	/** Get Line No.
+	  * Unique line for this document
+	  */
+	public int getLine();
+
+    /** Column name M_AttributeSetInstanceTo_ID */
+    public static final String COLUMNNAME_M_AttributeSetInstanceTo_ID = "M_AttributeSetInstanceTo_ID";
+
+	/** Set Attribute Info To.
+	  * Target Product Attribute Set Instance
+	  */
+	public void setM_AttributeSetInstanceTo_ID (int M_AttributeSetInstanceTo_ID);
+
+	/** Get Attribute Info To.
+	  * Target Product Attribute Set Instance
+	  */
+	public int getM_AttributeSetInstanceTo_ID();
+
+	public I_M_AttributeSetInstance getM_AttributeSetInstanceTo() throws RuntimeException;
+
+    /** Column name M_AttributeSetInstance_ID */
+    public static final String COLUMNNAME_M_AttributeSetInstance_ID = "M_AttributeSetInstance_ID";
+
+	/** Set Attribute Info.
+	  * Product Attribute Set Instance
+	  */
+	public void setM_AttributeSetInstance_ID (int M_AttributeSetInstance_ID);
+
+	/** Get Attribute Info.
+	  * Product Attribute Set Instance
+	  */
+	public int getM_AttributeSetInstance_ID();
+
+	public I_M_AttributeSetInstance getM_AttributeSetInstance() throws RuntimeException;
+
+    /** Column name M_LocatorTo_ID */
+    public static final String COLUMNNAME_M_LocatorTo_ID = "M_LocatorTo_ID";
+
+	/** Set Locator To.
+	  * Location inventory is moved to
+	  */
+	public void setM_LocatorTo_ID (int M_LocatorTo_ID);
+
+	/** Get Locator To.
+	  * Location inventory is moved to
+	  */
+	public int getM_LocatorTo_ID();
+
+	public org.compiere.model.I_M_Locator getM_LocatorTo() throws RuntimeException;
+
+    /** Column name M_Locator_ID */
+    public static final String COLUMNNAME_M_Locator_ID = "M_Locator_ID";
+
+	/** Set Locator.
+	  * Warehouse Locator
+	  */
+	public void setM_Locator_ID (int M_Locator_ID);
+
+	/** Get Locator.
+	  * Warehouse Locator
+	  */
+	public int getM_Locator_ID();
+
+	public org.compiere.model.I_M_Locator getM_Locator() throws RuntimeException;
+
+    /** Column name M_MovementLine_ID */
+    public static final String COLUMNNAME_M_MovementLine_ID = "M_MovementLine_ID";
+
+	/** Set Move Line.
+	  * Inventory Move document Line
+	  */
+	public void setM_MovementLine_ID (int M_MovementLine_ID);
+
+	/** Get Move Line.
+	  * Inventory Move document Line
+	  */
+	public int getM_MovementLine_ID();
+
+	public org.compiere.model.I_M_MovementLine getM_MovementLine() throws RuntimeException;
+
+    /** Column name M_Product_ID */
+    public static final String COLUMNNAME_M_Product_ID = "M_Product_ID";
+
+	/** Set Product.
+	  * Product, Service, Item
+	  */
+	public void setM_Product_ID (int M_Product_ID);
+
+	/** Get Product.
+	  * Product, Service, Item
+	  */
+	public int getM_Product_ID();
+
+	public org.compiere.model.I_M_Product getM_Product() throws RuntimeException;
+
+    /** Column name MovementQty */
+    public static final String COLUMNNAME_MovementQty = "MovementQty";
+
+	/** Set Movement Quantity.
+	  * Quantity of a product moved.
+	  */
+	public void setMovementQty (BigDecimal MovementQty);
+
+	/** Get Movement Quantity.
+	  * Quantity of a product moved.
+	  */
+	public BigDecimal getMovementQty();
+
+    /** Column name Processed */
+    public static final String COLUMNNAME_Processed = "Processed";
+
+	/** Set Processed.
+	  * The document has been processed
+	  */
+	public void setProcessed (boolean Processed);
+
+	/** Get Processed.
+	  * The document has been processed
+	  */
+	public boolean isProcessed();
+
+    /** Column name Updated */
+    public static final String COLUMNNAME_Updated = "Updated";
+
+	/** Get Updated.
+	  * Date this record was updated
+	  */
+	public Timestamp getUpdated();
+
+    /** Column name UpdatedBy */
+    public static final String COLUMNNAME_UpdatedBy = "UpdatedBy";
+
+	/** Get Updated By.
+	  * User who updated this records
+	  */
+	public int getUpdatedBy();
+}
---
+++ jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/model/I_JP_PP_MM_PlanLineT.java
@@ -0,0 +1,267 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package jpiere.base.plugin.org.adempiere.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import org.compiere.model.*;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for JP_PP_MM_PlanLineT
+ *  @author iDempiere (generated) 
+ *  @version Release 10
+ */
+@SuppressWarnings("all")
+public interface I_JP_PP_MM_PlanLineT 
+{
+
+    /** TableName=JP_PP_MM_PlanLineT */
+    public static final String Table_Name = "JP_PP_MM_PlanLineT";
+
+    /** AD_Table_ID=1000313 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 1 - Org 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(1);
+
+    /** Load Meta Data */
+
+    /** Column name AD_Client_ID */
+    public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/** Get Tenant.
+	  * Tenant for this installation.
+	  */
+	public int getAD_Client_ID();
+
+    /** Column name AD_Org_ID */
+    public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/** Set Organization.
+	  * Organizational entity within tenant
+	  */
+	public void setAD_Org_ID (int AD_Org_ID);
+
+	/** Get Organization.
+	  * Organizational entity within tenant
+	  */
+	public int getAD_Org_ID();
+
+    /** Column name Created */
+    public static final String COLUMNNAME_Created = "Created";
+
+	/** Get Created.
+	  * Date this record was created
+	  */
+	public Timestamp getCreated();
+
+    /** Column name CreatedBy */
+    public static final String COLUMNNAME_CreatedBy = "CreatedBy";
+
+	/** Get Created By.
+	  * User who created this records
+	  */
+	public int getCreatedBy();
+
+    /** Column name Description */
+    public static final String COLUMNNAME_Description = "Description";
+
+	/** Set Description.
+	  * Optional short description of the record
+	  */
+	public void setDescription (String Description);
+
+	/** Get Description.
+	  * Optional short description of the record
+	  */
+	public String getDescription();
+
+    /** Column name IsActive */
+    public static final String COLUMNNAME_IsActive = "IsActive";
+
+	/** Set Active.
+	  * The record is active in the system
+	  */
+	public void setIsActive (boolean IsActive);
+
+	/** Get Active.
+	  * The record is active in the system
+	  */
+	public boolean isActive();
+
+    /** Column name JP_CommunicationColumn */
+    public static final String COLUMNNAME_JP_CommunicationColumn = "JP_CommunicationColumn";
+
+	/** Set Communication Column	  */
+	public void setJP_CommunicationColumn (String JP_CommunicationColumn);
+
+	/** Get Communication Column	  */
+	public String getJP_CommunicationColumn();
+
+    /** Column name JP_PP_MM_PlanLineT_ID */
+    public static final String COLUMNNAME_JP_PP_MM_PlanLineT_ID = "JP_PP_MM_PlanLineT_ID";
+
+	/** Set JP_PP_MM_PlanLineT	  */
+	public void setJP_PP_MM_PlanLineT_ID (int JP_PP_MM_PlanLineT_ID);
+
+	/** Get JP_PP_MM_PlanLineT	  */
+	public int getJP_PP_MM_PlanLineT_ID();
+
+    /** Column name JP_PP_MM_PlanLineT_UU */
+    public static final String COLUMNNAME_JP_PP_MM_PlanLineT_UU = "JP_PP_MM_PlanLineT_UU";
+
+	/** Set JP_PP_MM_PlanLineT_UU	  */
+	public void setJP_PP_MM_PlanLineT_UU (String JP_PP_MM_PlanLineT_UU);
+
+	/** Get JP_PP_MM_PlanLineT_UU	  */
+	public String getJP_PP_MM_PlanLineT_UU();
+
+    /** Column name JP_PP_PlanT_ID */
+    public static final String COLUMNNAME_JP_PP_PlanT_ID = "JP_PP_PlanT_ID";
+
+	/** Set PP Plan Template.
+	  * JPIERE-0501:JPBP
+	  */
+	public void setJP_PP_PlanT_ID (int JP_PP_PlanT_ID);
+
+	/** Get PP Plan Template.
+	  * JPIERE-0501:JPBP
+	  */
+	public int getJP_PP_PlanT_ID();
+
+	public I_JP_PP_PlanT getJP_PP_PlanT() throws RuntimeException;
+
+    /** Column name Line */
+    public static final String COLUMNNAME_Line = "Line";
+
+	/** Set Line No.
+	  * Unique line for this document
+	  */
+	public void setLine (int Line);
+
+	/** Get Line No.
+	  * Unique line for this document
+	  */
+	public int getLine();
+
+    /** Column name M_AttributeSetInstanceTo_ID */
+    public static final String COLUMNNAME_M_AttributeSetInstanceTo_ID = "M_AttributeSetInstanceTo_ID";
+
+	/** Set Attribute Info To.
+	  * Target Product Attribute Set Instance
+	  */
+	public void setM_AttributeSetInstanceTo_ID (int M_AttributeSetInstanceTo_ID);
+
+	/** Get Attribute Info To.
+	  * Target Product Attribute Set Instance
+	  */
+	public int getM_AttributeSetInstanceTo_ID();
+
+	public I_M_AttributeSetInstance getM_AttributeSetInstanceTo() throws RuntimeException;
+
+    /** Column name M_AttributeSetInstance_ID */
+    public static final String COLUMNNAME_M_AttributeSetInstance_ID = "M_AttributeSetInstance_ID";
+
+	/** Set Attribute Info.
+	  * Product Attribute Set Instance
+	  */
+	public void setM_AttributeSetInstance_ID (int M_AttributeSetInstance_ID);
+
+	/** Get Attribute Info.
+	  * Product Attribute Set Instance
+	  */
+	public int getM_AttributeSetInstance_ID();
+
+	public I_M_AttributeSetInstance getM_AttributeSetInstance() throws RuntimeException;
+
+    /** Column name M_LocatorTo_ID */
+    public static final String COLUMNNAME_M_LocatorTo_ID = "M_LocatorTo_ID";
+
+	/** Set Locator To.
+	  * Location inventory is moved to
+	  */
+	public void setM_LocatorTo_ID (int M_LocatorTo_ID);
+
+	/** Get Locator To.
+	  * Location inventory is moved to
+	  */
+	public int getM_LocatorTo_ID();
+
+	public org.compiere.model.I_M_Locator getM_LocatorTo() throws RuntimeException;
+
+    /** Column name M_Locator_ID */
+    public static final String COLUMNNAME_M_Locator_ID = "M_Locator_ID";
+
+	/** Set Locator.
+	  * Warehouse Locator
+	  */
+	public void setM_Locator_ID (int M_Locator_ID);
+
+	/** Get Locator.
+	  * Warehouse Locator
+	  */
+	public int getM_Locator_ID();
+
+	public org.compiere.model.I_M_Locator getM_Locator() throws RuntimeException;
+
+    /** Column name M_Product_ID */
+    public static final String COLUMNNAME_M_Product_ID = "M_Product_ID";
+
+	/** Set Product.
+	  * Product, Service, Item
+	  */
+	public void setM_Product_ID (int M_Product_ID);
+
+	/** Get Product.
+	  * Product, Service, Item
+	  */
+	public int getM_Product_ID();
+
+	public org.compiere.model.I_M_Product getM_Product() throws RuntimeException;
+
+    /** Column name MovementQty */
+    public static final String COLUMNNAME_MovementQty = "MovementQty";
+
+	/** Set Movement Quantity.
+	  * Quantity of a product moved.
+	  */
+	public void setMovementQty (BigDecimal MovementQty);
+
+	/** Get Movement Quantity.
+	  * Quantity of a product moved.
+	  */
+	public BigDecimal getMovementQty();
+
+    /** Column name Updated */
+    public static final String COLUMNNAME_Updated = "Updated";
+
+	/** Get Updated.
+	  * Date this record was updated
+	  */
+	public Timestamp getUpdated();
+
+    /** Column name UpdatedBy */
+    public static final String COLUMNNAME_UpdatedBy = "UpdatedBy";
+
+	/** Get Updated By.
+	  * User who updated this records
+	  */
+	public int getUpdatedBy();
+}
