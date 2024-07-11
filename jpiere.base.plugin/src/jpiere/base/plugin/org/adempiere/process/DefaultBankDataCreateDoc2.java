ad97e9575a9a4f94ce56ee51c5e29252be756749
OES-MAIN-PC\hhagi <h.hagiwara@ha.hocoroco.ne.jp>
2024-07-11 18:12:07 +0900

    Improve JPIERE-0308 : Default Bank Data create Doc

--- jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/process/DefaultBankDataCreateDoc2.java
+++ jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/process/DefaultBankDataCreateDoc2.java
@@ -93,7 +93,12 @@ public class DefaultBankDataCreateDoc2 extends SvrProcess {
 				PO.copyValues(m_BankData, bs);
 				bs.setAD_Org_ID(m_BankData.getAD_Org_ID());
 				if(BDSchema.getJP_BankStatementDocType_ID() > 0)
+				{
 					bs.setC_DocType_ID(BDSchema.getJP_BankStatementDocType_ID());
+					MDocType m_DocType = MDocType.get(bs.getC_DocType_ID());
+					if(m_DocType.isDocNoControlled())
+						bs.setDocumentNo(DB.getDocumentNo(bs.getC_DocType_ID(), null, false, bs));
+				}
 				bs.saveEx(get_TrxName());
 
 				m_BankData.setC_BankStatement_ID(bs.getC_BankStatement_ID());
