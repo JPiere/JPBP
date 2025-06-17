04329b6d3a8ac07979af507c4101ba7a9065457d
OES-MAIN-PC\hhagi <h.hagiwara@compiere-distribution-lab.net>
2025-06-17 12:12:28 +0900

    Maintain JPIERE-0462,0468

--- jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/impexp/JPiereGridTabCSVImporter.java
+++ jpiere.base.plugin/src/jpiere/base/plugin/org/adempiere/impexp/JPiereGridTabCSVImporter.java
@@ -5,8 +5,12 @@
  * This program is free software, you can redistribute it and/or modify it    *
  * under the terms version 2 of the GNU General Public License as published   *
  * by the Free Software Foundation. This program is distributed in the hope   *
- * that it will be useful, but WITHOUT ANY WARRANTY.                          *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
  * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
  *                                                                            *
  * JPiere is maintained by OSS ERP Solutions Co., Ltd.                        *
  * (http://www.oss-erp.co.jp)                                                 *
@@ -257,7 +261,7 @@ public class JPiereGridTabCSVImporter implements IGridTabImporter
 						createTrx(gridTab);
 					}
 					if (trx != null)
-						trx.setDisplayName(JPiereGridTabCSVImporter.class.getName()+"_fileImport_" + gridTab.getTableName());
+						trx.setDisplayName(JPiereGridTabCSVImporter.class.getName()+"_fileImport_" + gridTab.getTableName());//JPIERE
 
 					String recordResult = processRecord(importMode, gridTab, indxDetail, isDetail, idx, rowResult, childs);
 					rowResult.append(recordResult);
